e7afd53c065f743b91716f3226e5b0195fba2ca2
andre.gawron <andre.gawron@ondemandcommerce.com>
2019-11-07 23:46:02 +0100

    added first drinks routes

---
+++ server/src/routes/drinks.js
@@ -0,0 +1,86 @@
+const _ = require('lodash');
+
+const router = require('express').Router();
+
+const drinkService = require('../drink').DrinkService;
+
+/**
+ * @swagger
+ * definition:
+ *   drink:
+ *     properties:
+ *       id:
+ *         type: string
+ *       details:
+ *         type: object
+ *         properties:
+ *          name:
+ *            type: string
+ *          image:
+ *            type: string
+ *          basePrice:
+ *            type: number
+ *            format: float
+ */
+const DrinkDto = drink => {
+  return {
+    id: drink.getId(),
+    details: drink.getDetails()
+  }
+};
+
+/**
+ * @swagger
+ * /api/drinks:
+ *   get:
+ *     description: Returns all drinks
+ *     produces:
+ *       - application/json
+ *     responses:
+ *       200:
+ *         description: An array of drinks
+ *         schema:
+ *           type: object
+ *           properties:
+ *             drinks:
+ *               type: array
+ *               description: list of drinks
+ *               items:
+ *                 $ref: '#/definitions/drink'
+ */
+router.get('/drinks/', (req, res, next) => {
+  res.status(200).json({
+    drinks: _.map(drinkService.list(), DrinkDto)
+  });
+});
+
+/**
+ * @swagger
+ * /api/drinks/{id}:
+ *   get:
+ *     description: Returns all drinks
+ *     produces:
+ *       - application/json
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         description: id of the drink to get
+ *         required: true
+ *         type: string
+ *     responses:
+ *       200:
+ *         description: An array of drinks
+ *         schema:
+ *           type: object
+ *           properties:
+ *             drink:
+ *               $ref: '#/definitions/drink'
+ *
+ */
+router.get('/drinks/:id', (req, res, next) => {
+  res.status(200).json({
+    drink: DrinkDto(drinkService.get(req.params.id))
+  })
+});
+
+module.exports = router;
