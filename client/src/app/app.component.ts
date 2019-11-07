27314339576a71d7c2eade0afec4a071a3cdb192
beckyarauz <rebeca.arauz@ondemandcommerce.com>
2019-11-07 15:35:47 +0100

    delete dist

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
-import { UserService } from './core/services/user.service';
+import { UserService } from './core/services';
 import {Router} from '@angular/router';
 
 @Component({
