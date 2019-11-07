ccba7ab627ea804adc0f1c2ef4796c9c45fa3b76
kasoban <git@kasoban.de>
2019-11-07 13:31:50 +0100

    linter fixes
    adding a bunch of semicolon

--- server/app.js
+++ server/app.js
@@ -83,19 +83,19 @@ app.get('*', (req, res) => {
 
 // Error handler
 app.use((err, req, res, next) => {
-  console.error("----- An error happened -----")
-  console.error(err)
+  console.error("----- An error happened -----");
+  console.error(err);
 
   // only render if the error ocurred before sending the response
   if (!res.headersSent) {
-    res.status(err.status || 500)
+    res.status(err.status || 500);
 
     // A limited amount of information sent in production
     if (process.env.NODE_ENV === 'production')
-      res.json(err)
+      res.json(err);
     else
       res.json(JSON.parse(JSON.stringify(err, Object.getOwnPropertyNames(err))))
   }
-})
+});
 
-module.exports = app
+module.exports = app;
