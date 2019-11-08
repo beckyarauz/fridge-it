531abcf01997556b786d278e02cb21d55fc675f4
andre.gawron <andre.gawron@ondemandcommerce.com>
2019-11-08 16:17:04 +0100

    added anonymous role

--- server/src/roles.js
+++ server/src/roles.js
@@ -7,10 +7,14 @@ module.exports = {
     'fridge_refill': true,
   },
   user: {
-    'drinks_list': false,
+    'drinks_list': true,
     'drinks_add': false,
 
     'fridge_list': true,
     'fridge_refill': true,
+  },
+  anonymous: {
+    'drink_list': true,
+    'fridge_list': true
   }
 };
